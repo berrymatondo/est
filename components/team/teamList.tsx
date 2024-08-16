39d9cfb09ce6263f89c8843f579d7878e6cd0db4
jmatondo <j_matondo@hotmail.com>
2024-08-17 01:46:49 +0200

    comm

--- components/team/teamList.tsx
+++ components/team/teamList.tsx
@@ -1,14 +1,53 @@
 import Image from "next/image";
 import React from "react";
-import im1 from "../../public/est2.jpg";
+import neyla from "../../public/neyla.png";
+import loic2 from "../../public/loic2.png";
 import { Phone } from "lucide-react";
 import { MdEmail } from "react-icons/md";
+import { Separator } from "@radix-ui/react-dropdown-menu";
 
 const TeamList = () => {
   return (
-    <div className="flex justify-center ">
-      <div className="relative border overflow-hidden h-[450px]  w-[250px] rounded-lg flex flex-col items-center justify-center">
-        <div className=" bg-gradient-to-tr from-white to-sky-100 h-[225px] w-[250px] rounded-br-3xl flex justify-center items-center">
+    <div className="flex flex-col justify-center px-2 gap-4 ">
+      <div className="grid grid-cols-4 gap-2 md:gap-4">
+        <div>
+          {" "}
+          <div className="auto-cols-max	bg-red-400 col-span-1 max-h:w-20	 max-md:w-20  rounded-full overflow-hidden">
+            <Image src={neyla} alt="fond" className="flex" />
+          </div>
+        </div>
+        <div className="col-span-3">
+          <p className="max-md:text-sm mb-1">
+            Neyla <span className="font-semibold">Dewez-Mudi</span>
+          </p>
+          <p className="max-md:text-xs">
+            {
+              "J'aime travailler avec les enfants et les adolescents. Mon souhait est d'offrir un espace où les possibilités s'ouvrent à nouveau."
+            }
+          </p>
+        </div>
+      </div>
+      <Separator />
+      <div className="grid grid-cols-4 gap-2 md:gap-4">
+        <div>
+          {" "}
+          <div className="auto-cols-max	bg-red-400 col-span-1 max-h:w-20	 max-md:w-20  rounded-full overflow-hidden">
+            <Image src={loic2} alt="fond" className="flex" />
+          </div>
+        </div>
+        <div className="col-span-3">
+          <p className="max-md:text-sm mb-1">
+            Loïc <span className="font-semibold">Dewez</span>
+          </p>
+          <p className="max-md:text-xs">
+            {
+              "En tant qu’ostéopathe diplômé D.O, spécialisé en maternité et chez les nourrissons, ma mission est de m’assurer que chez bébé ainsi que la maman, les structures et les tissus du corps soient parfaitement mobiles, libres et fonctionnent comme ils ont été créés pour le faire.J’exerce depuis 10 ans et je suis heureux de pouvoir vous aider et vous guider."
+            }
+          </p>
+        </div>
+      </div>
+      {/*       <div className="relative border overflow-hidden h-[450px]  w-[300px] rounded-lg flex flex-col items-center justify-center">
+        <div className=" bg-gradient-to-tr from-white to-sky-100 h-[225px] w-[300px] rounded-br-3xl flex justify-center items-center">
           <div className="max-md:w-40 rounded-full overflow-hidden">
             <Image
               src={im1}
@@ -19,7 +58,7 @@ const TeamList = () => {
             />
           </div>
         </div>
-        <div className="bg-sky-600 h-[225px] w-[275px] rounded-tl-3xl flex flex-col text-white items-center p-4 gap-2">
+        <div className="bg-sky-600 h-[225px] w-[300px] rounded-tl-3xl flex flex-col text-white items-center p-4 gap-2">
           <p>
             Laetitia <strong>MATONDO</strong>
           </p>
@@ -33,7 +72,7 @@ const TeamList = () => {
         </div>
         <div className="bg-white -z-10 absolute left-0 h-[450px] w-[150px]"></div>
         <div className="bg-sky-600 -z-10 absolute right-0 h-[450px] w-[150px]"></div>
-      </div>
+      </div> */}
     </div>
   );
 };
